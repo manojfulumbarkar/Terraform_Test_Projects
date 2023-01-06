c5ee88e3dcb540d1c16bbd3ba737daa33142aaa7
manojfulumbarkar <100022849+manojfulumbarkar@users.noreply.github.com>
2023-01-06 16:20:47 +0530

    Add files via upload

---
+++ git/main.tf
@@ -0,0 +1,21 @@
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+
+##project_vpc
+###vpc
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = null
+  enable_dns_support   = null
+
+  tags = {
+    "Name" = "myvpc"
+  }
+
+}
